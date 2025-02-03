7b97ee626ebf3f4a1b433caea2219636d4808f6e
Ines Louro <inespazlouro@gmail.com>
2025-02-03 09:45:37 -0300

    added content with placeholder images

---
+++ src/content/books.js
@@ -0,0 +1,106 @@
+import alemDaDemocracia from '@/images/books/para-alem-da-democracia.png';
+import alemDaDemocraciaLateral from '@/images/books/para-alem-da-democracia-lateral.png';
+import gracasADeus from '@/images/books/gracas-a-deus-pelo-bitcoin.png';
+import gracasADeusLateral from '@/images/books/gracas-a-deus-pelo-bitcoin-lateral.png';
+import precoAmanha from '@/images/books/o-preco-do-amanha.png';
+import precoAmanhaLateral from '@/images/books/o-preco-do-amanha-lateral.png';
+import teseAscencao from '@/images/books/a-tese-da-ascencao-do-bitcoin.png';
+import teseAscencaoLateral from '@/images/books/a-tese-da-ascencao-do-bitcoin-lateral.png';
+import tudoDividido21Milhoes from '@/images/books/tudo-dividido-por-21-milhoes-lateral.png';
+import tudoDividido21MilhoesLateral from '@/images/books/tudo-dividido-por-21-milhoes-lateral.png';
+import vinteUmaLicoes from '@/images/books/21-licoes.png';
+import vinteUmaLicoesLateral from '@/images/books/21-licoes-lateral.png';
+
+export default books = [
+  {
+    title: '21 Lições',
+    description: [
+      '**O que é Bitcoin?** Esta pergunta inócua é surpreendentemente difícil de responder. Bitcoin é uma rede informática, uma nova forma de dinheiro, um sistema de pagamento resistente à censura, uma revolução financeira, uma forma pacífica de protesto, e muito mais.',
+      'Mas este livro não procura responder a essa pergunta. Tenta, sim, responder a outra pergunta, mais pessoal: **O que aprendi eu com Bitcoin?**',
+      'As **21 Lições** estão estruturadas em grupos de sete, resultando em três capítulos. Cada um analisa a Bitcoin por uma lente diferente, extraindo as que podem ser aprendidas através da inspeção desta estranha rede a partir de um ângulo diferente.',
+    ],
+    authorDescription: [
+      'Gigi é um bitcoiner e engenheiro de software, escreve palavras e código para Bitcoin. O seu primeiro livro, 21 Lições, é um dos livros mais recomendados entre os bitcoiners. Lançou vários projectos no espaço Bitcoin e está actualmente a trabalhar no seu segundo livro, 21 Ways. O seu foco principal é a privacidade, segurança, software livre, e educação.',
+    ],
+    cover: vinteUmaLicoes,
+    lateralCover: vinteUmaLicoesLateral,
+  },
+  {
+    title: 'Graças a Deus pelo Bitcoin',
+    description: [
+      'O dinheiro faz parte do nosso dia-a-dia. Ganhamo-lo, gastamo-lo e poupamo-lo. Veneramo-lo e confiamos nele para suprir as nossas necessidades. Embora muito já tenha sido escrito sobre o poder, o perigo e a gestão do dinheiro, pouco foi ainda escrito sobre o que ele é, bem como da sua moralidade. Essa lacuna de informação tem sido aproveitada para enriquecer alguns privilegiados, escravizar milhões e introduzir desordem e divisão em todo o mundo. Como é que isto aconteceu e o que podemos fazer para o remediar?',
+      '**Graças a Deus pelo Bitcoin** analisa as formas pelas quais o sistema monetário atual se encontra quebrado e o que pode ser feito para consertá-lo. Este trabalho examina a criação de dinheiro e a sua corrupção e potencial redenção, além de explorar como o bitcoin está a resgatar os males do nosso sistema monetário corrupto e o quanto a transição em curso para o dinheiro sólido é uma fonte de esperança para um mundo fraturado.',
+    ],
+    authorDescription: [
+      '**Derek Waltchack** descobriu o Bitcoin em 2015 e tem estado obcecado desde então. Em 2005 co-fundou Shannon Waltchack, uma empresa de corretagem e investimento imobiliário comercial de serviço completo. Derek cresceu no coração da Igreja de Belmont de Tennessee-Nashville, no coração do Cinturão Bíblico.',
+      '**Gabe Higgins** é um empresário, músico, tecnólogo e libertário. Cresceu numa igreja Pentecostal em Clearwater, Florida, e, na sua adolescência, começou a tocar bateria e guitarra na banda dos jovens, de louvor e adoração. Gabe continua a servir como músico na sua atual igreja matriz, bem como na sua banda de hard rock baseada na fé. Ele descobriu o Bitcoin em finais de 2012 logo após ter aprendido com o Dr. Ron Paul sobre o papel da banca central na política monetária.',
+      '**George Mekhail** é um bitcoiner e antigo Pastor Executivo. Nascido no Cairo, Egipto, cresceu servindo como diácono na Igreja Coptic Orthodox. Foi cofundador de uma organização chamada Church Clarity, que insiste que os pastores sejam francos nas suas políticas, quer teologicamente conservadoras, quer progressistas.',
+      "**Jimmy Song** é um programador de Bitcoin e libertariano cristão. Nasceu em Seul, Coreia do Sul, onde a sua devota avó materna lhe apresentou o Evangelho. Frequentou uma grande variedade de igrejas, desde Vineyard e Hillsong até Presbyterian e Southern Baptist, e muitas entre elas. Escreveu dois outros livros, Programming Bitcoin, publicado por O'Reilly e The Little Bitcoin Book, com edição de autor.",
+      '**J. M. Bush** é um plantador de igrejas atualmente a servir em Montevideo, Uruguai. Cresceu numa família cristã e chegou à fé ainda jovem, mas só nos seus últimos anos de liceu é que veio a abraçar as implicações do Senhorio de Cristo sobre todas as coisas. O estudo da filosofia económica acabou por levá-lo para a toca do coelho Bitcoin.',
+      '**Julia Tourianski** é uma comentadora política e YouTuber. Bitcoin cativou o seu interesse porque se alinhou com as suas crenças económicas e políticas. Ela escreveu The Declaration of Bitcoin’s Indepedence e continua a falar sobre a natureza vital desta tecnologia.',
+      '**Lyle Pratt** é um investidor, programador, empresário e um “nerd” da economia. Cresceu em igrejas baptistas do Sul, mas desde então migrou para outros espaços. É um membro fundador da Igreja Paz de Cristo em Round Rock, TX, onde a sua esposa, Aurelia, serve como seu pastor principal.',
+      '**Robert Breedlove** é um empresário, escritor e filósofo com foco em Bitcoin. Foi criado no Tennessee, frequentando igrejas baptistas do Sul, mas passou a maior parte da sua vida espiritual ainda agnóstico. Através das suas explorações na proverbial "toca do coelho Bitcoin", Robert viu-se familiarizado com o cristianismo aos 33 anos de idade. Robert considera-se um Maximalista da Liberdade e acredita ter encontrado o trabalho da sua vida ao redor do Bitcoin dando o seu contributo para a separar o dinheiro do estado.',
+    ],
+    cover: gracasADeus,
+    lateralCover: gracasADeusLateral,
+  },
+  {
+    title: 'A Tese da Ascensão do Bitcoin',
+    description: [
+      '**A Tese da Ascensão do Bitcoin** é uma leitura informativa e cativante, tanto para o recém-chegado como para o entusiasta de longa data do Bitcoin. Boyapati faz um caso convincente para o Bitcoin através de uma rápida visita ao cenário histórico do dinheiro, às propriedades de diferentes tipos de dinheiro e à razão pela qual o Bitcoin é a melhor forma de dinheiro e reserva de valor do mundo, potencialmente destinada a deslocar todas as outras.',
+      'Este livro representa um tour de force intelectual entregue com elegância e presciência por um polímata bem versado em matemática, informática, economia, filosofia, política, e engenharia.',
+    ],
+    authorDescription: [
+      'Nascido e criado na Austrália, **Vijay Boyapati** mudou-se para os Estados Unidos em 2000 para frequentar um Doutoramento em Ciências da Computação. Em vez de se inscrever no doutoramento, Boyapati acabou numa pequena startup chamada Google, onde passou vários anos a usar a sua formação em aprendizagem automática para melhorar os algoritmos de classificação usados na Google News. Em 2007, Boyapati deixou o seu emprego bem pago para trabalhar numa campanha popular para as eleições presidenciais de 2008, ajudando a arrecadar milhões de dólares e a trazer centenas de voluntários a New Hampshire para apoiar Ron Paul. Em 2011, Boyapati descobriu o bitcoin e empreendeu uma fantástica viagem para tentar compreender como é que uma nova forma de dinheiro baseada na internet, sem o suporte de qualquer bem físico nem a garantia de qualquer governo, poderia ter algum tipo de valor económico. Com uma formação base em economia austríaca, Boyapati escreveu **A tese da ascensão do Bitcoin** em formato de artigo longo em 2017, oferecendo um enquadramento económico para que qualquer leigo na matéria pudesse compreender o funcionamento do bitcoin.',
+    ],
+    cover: teseAscencao,
+    lateralCover: teseAscencaoLateral,
+  },
+  {
+    title: 'O Preço do Amanhã',
+    description: [
+      '**Vivemos numa época extraordinária.** Os avanços tecnológicos estão a acontecer mais depressa do que a nossa capacidade para os compreender e, num mundo que está a avançar a um ritmo mais rápido do que podemos imaginar, não nos podemos dar ao luxo de ficar parados.',
+      'Precisamos de construir uma nova estrutura para as nossas economias locais e globais, e temos de o fazer depressa. Senão, a mesma tecnologia que tem a capacidade de trazer abundância, para nós e para o nosso mundo, irá simplesmente destruí-lo.',
+      'Neste livro extraordinariamente inconformista, Jeff Booth, líder e CEO nos setores do comércio eletrónico e da tecnologia, explica em detalhe as realidades tecnológicas e económicas que moldam o nosso mundo, presente e futuro, e as escolhas que fazemos à medida que avançamos – uma situação potencialmente alarmante, mas extremamente promissora.',
+    ],
+    authorDescription: [
+      '**Jeff Booth** é um líder visionário que viveu na vanguarda da transição tecnológica durante 20 anos. Liderou a BuildDirect – uma empresa tecnológica que tinha como missão simplificar o setor da construção – ao longo de quase duas décadas, passando pelo colapso do setor das dotcom, pela crise financeira de 2008 e por diversas ondas de disrupção tecnológica.Jeff foi mencionado na Forbes, TechCrunch, Inc.com, The Globe and Mail, BNN, Fast Company, Entrepreneur, Bloomberg, TIME e The Wall Street Journal. Em 2015, foi nomeado Pessoa do Ano pela BC Technology Industry Association’s (BCTIA) e em 2016 a Goldman Sachs classificou-o entre os seus 100 empreendedores mais intrigantes.',
+    ],
+    cover: precoAmanha,
+    lateralCover: precoAmanhaLateral,
+  },
+  {
+    title: 'Para Além da Democracia',
+    description: [
+      '"Acabei de o ler. Excelente. Ótimo trabalho. Parabéns." Hans-Hermann Hoppe, autor de "Democracia: O Deus Que Falhou"',
+      'A democracia é amplamente considerada como o melhor sistema político que se pode imaginar. Não é certamente um exagero dizer que a democracia se tornou uma religião secular. A maior fé política do mundo. Criticar o ideal democrático é arriscar-se a ser considerado um inimigo da sociedade civilizada.',
+      'É precisamente isso que Karel Beckman e Frank Karsten se propõem fazer. Neste livro provocador e de leitura fácil, abordam o derradeiro tabu político: a ideia de que a nossa salvação reside na democracia',
+      'Com argumentos simples e diretos, mostram que a democracia, ao contrário do que se pensa, não conduz à liberdade, à civilização, à prosperidade, à paz e ao direito, mas sim ao oposto: perda de liberdade, conflitos sociais, desperdício de dinheiro público, diminuição do nível de vida e subversão dos direitos individuais.',
+      'Em apenas 122 páginas, desmascaram 13 grandes mitos com que a democracia é habitualmente defendida. Para além disso, oferecem uma alternativa apelativa: uma sociedade baseada na liberdade individual e em relações sociais voluntárias.',
+      'Já alguma vez se perguntou porque é que o governo continua a crescer e a dívida pública é cada vez maior, enquanto a sua liberdade e prosperidade parecem estar cada vez mais ameaçadas? Depois de ler este livro, deixará de se interrogar - saberá porque é que isto está a acontecer e o que pode ser feito.',
+      'Para Além da Democracia é um livro inovador e fascinante para todos os que querem compreender melhor os atuais problemas sociais e a crise económica.',
+    ],
+    authorDescription: [
+      '**Frank Karsten** é o fundador do Mises Instituut Nederland e da Stichting Meer Vrijheid (Fundação Mais Liberdade), duas organizações libertárias neerlandesas que contribuem para o aumento da liberdade pessoal e económica. Aparece regularmente em público, para se pronunciar contra a crescente interferência do Estado na vida dos cidadãos. www.mises.nl.',
+      '**Karel Beckman** é escritor e jornalista. É fundador e editor-chefe da publicação digital independente Energy Post (www.energypost.eu). Antes disso, trabalhou como jornalista, no jornal financeiro neerlandês Financieele Dagblad. O seu site pessoal é www.charlieville.nl.',
+    ],
+    cover: alemDaDemocracia,
+    lateralCover: alemDaDemocraciaLateral,
+  },
+  {
+    title: 'Tudo Dividido por 21 Milhões',
+    description: [
+      'Estamos todos acorrentados à seta implacável do tempo. Nascemos, vivemos e envelhecemos. Pelo menos é isso que esperamos que aconteça. Depois morremos.',
+      'Nenhum de nós, humanos, é milionário em termos de horas, mas sacrificamos o nosso bem mais valioso e escasso apenas para nos mantermos vivos na roda do hamster mais um dia.',
+      'Na tentativa de poupar tempo, Satoshi deparou-se com a descoberta de um novo elemento, sem peso e absolutamente escasso. O travão para todo e qualquer ladrão de tempo. Elemento zero. Bitcoin.',
+      'O limite de quantidade do Bitcoin é o objeto imóvel que nos permite a todos tornarmo-nos uma força imparável.',
+      '“Porque a verdade é que o sistema somos nós. Cada uma das nossas ações de escolha, por sua vez, muda o nosso mundo. É assim que as ideias que outrora não conseguíamos entender se tornam realidade. Uma pessoa de cada vez.”',
+      '— Do prefácio de Jeff Booth',
+    ],
+    authorDescription: [
+      "**Knut Svanholm** is a Bitcoin author, educator, and armchair philosopher. His first work, 'Bitcoin: Sovereignty through Mathematics,' is widely regarded as a classic in the space, earning recognition as a recommended read on numerous educational platforms, including MicroStrategy's Bitcoin site hope.com. Knut is credited with coining the popular meme 'Everything Divided by 21 Million,' a phrase that encapsulates the scarcity and mathematical principles underpinning Bitcoin. Knut is also the host of The Bitcoin Infinity Show.",
+    ],
+    cover: tudoDividido21Milhoes,
+    lateralCover: tudoDividido21MilhoesLateral,
+  },
+];
