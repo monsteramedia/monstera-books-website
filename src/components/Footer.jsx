e61d0a6906b4e880de8cf153205b12476bb7f6f5
Ines Louro <inespazlouro@gmail.com>
2025-02-04 10:28:07 -0300

    enhanced footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,8 +7,8 @@ import logo from '@/images/monstera-books-logo-white.png';
 export function Footer() {
   return (
     <nav className='bg-salmon py-12'>
-      <Container className='flex flex-col items-center justify-center md:justify-between md:flex-row gap-4 md:gap-0'>
-        <div className='flex items-center flex-col md:flex-row gap-4'>
+      <Container className='flex flex-col items-center justify-center md:justify-between md:flex-row gap-8 md:gap-0'>
+        <div className='flex items-center flex-col md:items-start lg:flex-row gap-4'>
           <Image src={logo} alt='Monstera Books' className='w-48 md:w-64' />
           <div className='flex flex-col justify-center text-center md:text-start gap-4'>
             <p className='text-white w-72'>
@@ -20,12 +20,14 @@ export function Footer() {
         </div>
         <div className='flex flex-col justify-center md:items-end gap-8'>
           <Socials variant='secondary'/>
-          <Button
-            variant='secondary'
-            href='https://shop.freemadeira.org/collections/monstera-books'
-          >
-            LOJA
-          </Button>
+          <div className='flex items-center justify-center'>
+            <Button
+              variant='secondary'
+              href='https://shop.freemadeira.org/collections/monstera-books'
+            >
+              LOJA
+            </Button>
+          </div>
         </div>
       </Container>
     </nav>
