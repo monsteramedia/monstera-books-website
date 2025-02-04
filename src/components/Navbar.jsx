58549a9de6dfcac0d90d57d609ec04f7521fbc74
Ines Louro <inespazlouro@gmail.com>
2025-02-03 22:20:34 -0300

    removed description from navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,10 +8,6 @@ export function Navbar() {
     <nav className='bg-black'>
       <Container className='flex gap-8 items-center justify-between h-36 relative'>
         <Image src={logo} alt='Monstera Books' className='w-48 md:w-64' />
-        <p className='text-white text-sm hidden md:block'>
-          Livros em português sobre economia, liberdade, literacia financeira,
-          tecnologia e Bitcoin.
-        </p>
         <Button href='https://shop.freemadeira.org/collections/monstera-books'>
           LOJA
         </Button>
