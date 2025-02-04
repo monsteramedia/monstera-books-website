41d1a6ffdbd616dbafc8aa61d4fcd110056869fe
Ines Louro <inespazlouro@gmail.com>
2025-02-03 22:19:39 -0300

    added button variant

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,26 +1,34 @@
-import Link from 'next/link'
+import Link from 'next/link';
 import { classNames } from '@/utils/functions';
 
-export const Button = ({ href, className, ...props }) => {
+export const Button = ({ href, className, variant, ...props }) => {
+  let buttonColorStyle =
+    'border-salmon text-salmon hover:bg-salmon hover:text-white';
+
+  switch (variant) {
+    case 'secondary':
+      buttonColorStyle =
+        'border-white text-white hover:bg-white hover:text-salmon';
+      break;
+    default:
+      break;
+  }
+
+  className = classNames(
+    'border border-2 font-semibold py-2 px-8 rounded-md transition-colors duration-500',
+    className,
+    buttonColorStyle
+  );
+
   return href ? (
     <Link
       href={href}
       target='_blank'
       rel='noopener noreferrer'
-      className={classNames(
-        'text-white border border-2 border-white font-semibold py-2 px-8 rounded-sm hover:bg-white hover:text-black transition duration-300',
-        className
-      )}
+      className={className}
       {...props}
     />
   ) : (
-    <button
-      type='button'
-      className={classNames(
-        'text-white border border-2 border-white font-semibold py-2 px-8 rounded-sm hover:bg-white hover:text-black transition duration-300',
-        className
-      )}
-      {...props}
-    />
+    <button type='button' className={className} {...props} />
   );
-}
+};
