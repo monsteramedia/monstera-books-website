67c19586e6b440edffc99d1f9ecea701084c7e7d
Ines Louro <inespazlouro@gmail.com>
2025-02-07 20:41:09 -0300

    modal style

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,6 +1,3 @@
-'use client'
-
-import { useState } from 'react'
 import { Dialog, DialogBackdrop, DialogPanel, DialogTitle } from '@headlessui/react'
 import { Button } from './Button'
 import Image from 'next/image'
@@ -15,44 +12,58 @@ export function Modal({ book, show, setShow }) {
       />
 
       <div className='fixed inset-0 z-10 w-screen'>
-        <div className='flex min-h-full items-end justify-center p-4 sm:items-center sm:p-0'>
+        <div className='flex min-h-full items-center justify-center p-4 sm:items-center'>
           <DialogPanel
             transition
-            className='overflow-y-auto max-h-[80vh] relative transform overflow-hidden rounded-lg bg-white px-4 pb-4 pt-5 text-left shadow-xl transition-all data-[closed]:translate-y-4 data-[closed]:opacity-0 data-[enter]:duration-300 data-[leave]:duration-200 data-[enter]:ease-out data-[leave]:ease-in sm:my-8 sm:w-full sm:max-w-2xl sm:p-6 data-[closed]:sm:translate-y-0 data-[closed]:sm:scale-95'
+            className='relative transform overflow-hidden rounded-lg bg-white px-4 pb-4 pt-5 text-left shadow-xl transition-all data-[closed]:translate-y-4 data-[closed]:opacity-0 data-[enter]:duration-300 data-[leave]:duration-200 data-[enter]:ease-out data-[leave]:ease-in sm:my-8 h-[80vh] sm:w-full sm:max-w-5xl sm:p-6 data-[closed]:sm:translate-y-0 data-[closed]:sm:scale-95'
           >
-            <div>
-              <div className='text-center space-y-2'>
-                <DialogTitle as='h3' className='text-xl font-semibold py-2'>
-                  <Image
-                    src={book.cover}
-                    alt={book.title}
-                    width={100}
-                    height={150}
-                    className='mx-auto mb-6'
-                  />
+            <div className='flex flex-col sm:flex-row gap-8 justify-center items-center h-full'>
+              <div className='flex flex-col items-center justify-center h-full hidden sm:flex'>
+                <Image
+                  src={book.cover}
+                  alt={book.title}
+                  className='min-w-56 mb-4'
+                />
+                <Button href={book.storeUrl} className='w-full text-center'>
+                  Comprar
+                </Button>
+              </div>
+              <div className='flex flex-col items-center text-center space-y-2 h-full overflow-y-auto scrollbar-hide'>
+                <DialogTitle
+                  as='h3'
+                  className='sticky top-0 bg-white text-xl font-semibold py-4 w-full'
+                >
                   {book.title}
                 </DialogTitle>
+                <Image
+                  src={book.cover}
+                  alt={book.title}
+                  className='max-w-40 block sm:hidden pb-8'
+                />
                 {book.description.map((description, index) => (
                   <div
-                    key={index}
+                    key={`description-${index}`}
                     className='text-sm font-light text-grey text-justify'
                   >
                     <ReactMarkdown>{description}</ReactMarkdown>
                   </div>
                 ))}
-                <p className='text-base font-medium pt-4 pb-2'>Sobre o autor</p>
+                <p className='text-base font-medium pt-8 pb-4'>Sobre o autor</p>
                 {book.authorDescription.map((description, index) => (
                   <div
-                    key={index}
-                    className='text-sm font-light text-grey text-justify'
+                    key={`author-${index}`}
+                    className='text-sm font-light text-grey text-justify last:mb-8'
                   >
                     <ReactMarkdown>{description}</ReactMarkdown>
                   </div>
                 ))}
+                <Button
+                  href={book.storeUrl}
+                  className='w-full text-center sticky bottom-0 bg-white sm:hidden'
+                >
+                  Comprar
+                </Button>
               </div>
-            </div>
-            <div className='mt-5 sm:mt-6 flex justify-center pt-2'>
-              <Button href={book.storeUrl}>Comprar</Button>
             </div>
           </DialogPanel>
         </div>
