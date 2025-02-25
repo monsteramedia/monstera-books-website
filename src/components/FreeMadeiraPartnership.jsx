d71589fd846096e16a524ff1d7e4980b6334a71e
Ines Louro <inespazlouro@gmail.com>
2025-02-25 17:45:42 -0500

    smaller free madeira partnership section

--- src/components/FreeMadeiraPartnership.jsx
+++ src/components/FreeMadeiraPartnership.jsx
@@ -5,19 +5,21 @@ import { Container } from '@/components/Container';
 
 export function FreeMadeiraPartnership() {
   return (
-    <Container className='h-[calc(100vh-144px)] mx-auto w-screen flex flex-col items-center justify-center text-center gap-4 md:gap-6'>
+    <Container className='h-[calc(100vh-144px)] mx-auto w-screen flex flex-col items-center justify-center text-center gap-8 md:gap-6'>
       <Image
         src={freeMadeira}
         alt='FREE Madeira and Monstera Books'
-        className='w-72 md:w-80 pb-4 md:pb-8'
+        className='w-60 md:w-64 pb-4 md:pb-8'
       />
-      <p className='text-black text-2xl md:text-4xl'>
-        Tudo sobre Bitcoin, pela FREE Madeira e Monstera Books
-      </p>
-      <p className='text-grey font-light text-base md:text-xl'>
-        De livros a hardware, roupa a acessórios, encontre aqui os seus
-        essenciais de Bitcoin.
-      </p>
+      <div className="flex flex-col gap-4 md:gap-6">
+        <p className='text-black text-xl md:text-2xl'>
+          Tudo sobre Bitcoin, pela FREE Madeira e Monstera Books
+        </p>
+        <p className='text-grey font-light text-sm md:text-base'>
+          De livros a hardware, roupa a acessórios, encontre aqui os seus
+          essenciais de Bitcoin.
+        </p>
+      </div>
       <Button
         href='https://shop.freemadeira.org/collections/monstera-books'
         className='mt-4'
